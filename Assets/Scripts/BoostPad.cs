0f6ae3553fd18330465d8b6c7de192d3a7eca2e6
Mike-n-ike1337 <michael.donovan@digipen.edu>
2022-02-07 12:47:45 -0800

    Script progress

--- Assets/Scripts/BoostPad.cs
+++ Assets/Scripts/BoostPad.cs
@@ -8,10 +8,18 @@ using UnityEngine;
 //Turns an object into a "boost pad", which launches the player at a specified velocity and direction.
 public class BoostPad : MonoBehaviour
 {
+    private Collision2D myCollision;
+    // Player is declared by the operator
+    [Tooltip("Define the player here")]
+    public Rigidbody2D playerRb;
+    [Tooltip("This variable will adjust the velocity that the pad will apply to the player.")]
+    public float boostforce;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCollision = GetComponent<Collision2D>();
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,4 +27,6 @@ public class BoostPad : MonoBehaviour
     {
         
     }
+
+
 }
